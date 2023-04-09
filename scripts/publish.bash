f904af25692e5a0292295e65caaa513d52f332f9
wyf-ACCEPT <define_wifi@163.com>
2023-04-09 13:39:45 +0800

    add publish commands

--- scripts/publish.bash
+++ scripts/publish.bash
@@ -1,6 +1,11 @@
+# Build and test the packages.
+sui move build
+sui move test
+
 # We recommend you to publish the meson contracts package using sui client.
 sui console
 
-# --- You're in sui console now.
-# cd [TODO]
+# --- You're in sui console now. If you don't have a wallet, use `new-address -h` to learn more.
 publish --gas-budget 99999
+
+# You can view the generated objects via explorer (https://explorer.sui.io/).
